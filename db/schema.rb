171d6ebce64d12f972b9464daa68aa84a0ca84e7
Vitthal Gaikwad <vitthal.gaikwad@cuelogic.co.in>
2015-06-29 12:21:30 +0530

    Generated scaffold for author.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626123222) do
+ActiveRecord::Schema.define(version: 20150629064345) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
